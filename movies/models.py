ab1ee494aa0229a05e0106319a1cef3c5fedbbaa
김광용 <veta6287@naver.com>
2022-05-19 11:20:56 +0900

    design model_1

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Movie(models.Model):
+    movie_id = models.IntegerField()
+    title = models.CharField(max_length=100)
+    original_title = models.CharField(max_length=100)
+    overview = models.TextField()
+    release_date = models.DateTimeField()
+    poster_path = models.CharField(max_length=100)
+    vote_average = models.FloatField()
+    genre = models.CharField(max_length=50)
+    like_user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
+
+class Comments(models.Model):
+
+    content = models.TextField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
